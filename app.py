2fc1a0d49d26ed45e93df3737233b6d8521eb9ea
kelvin Jung <developer.kelvin@gmail.com>
2021-02-23 16:47:17 +0900

    baseline

--- app.py
+++ app.py
@@ -1,19 +1,106 @@
-from flask import Flask, request, make_response
+from flask import Flask, request, make_response, render_template, session
+from flask.helpers import url_for
+from werkzeug.utils import redirect
 from sp import *
 import pymysql
 
 app = Flask(__name__)
 
-@app.route('/')
-def hello_world():
-    return "hello world"
+app.secret_key = b'_kelvinkelvin_'
 
-@app.route('/login')
-def wow():
-    return "login page"
+@app.route('/')
+def index():
+    return render_template('index.html')
+
+@app.route('/workspace')
+def workspace():
+    if 'user_id' in session:
+        workspaces = sp_get_user_workspace(session['user_id'])
+        return render_template('workspace.html',workspaces=workspaces)
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/addworkspace', methods=['GET', 'POST'])
+def add_workspace():
+    if 'user_id' in session:
+        if request.method == 'GET':
+            return render_template('add_workspace.html')
+        elif request.method == 'POST':
+            name = request.form['name']
+            desc = request.form['desc']
+            is_success = sp_add_workspace(session['user_id'], name, desc)
+            if is_success:
+                return redirect(url_for('workspace'))
+            else:
+                return render_template('add_workspace.html')
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/registserver', methods=['GET', 'POST'])
+def regist_server():
+    if 'user_id' in session:
+        if request.method == 'GET':
+            return render_template('regist_server.html')
+        elif request.method == 'POST':
+            # do_something
+            raise NotImplementedError()
+            return redirect(url_for('workspace'))
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/dashboard')
+def dashboard():
+    if 'user_id' in session:
+        return render_template('dashboard.html')
+    else:
+        return redirect(url_for('login'))
+
+@app.route('/signup', methods=['GET', 'POST'])
+def signup():
+    error = None
+    if request.method == 'GET':
+        # render login page
+        return render_template('signup.html')
+    else: 
+        id = request.form['id'] 
+        pwd = request.form['pwd']
+        name = request.form['name']
+        email = request.form['email']
+        desc = request.form['desc']
+
+        is_signup_success = sp_signup(id, pwd, name, email, desc)
+
+        if is_signup_success:
+            return redirect(url_for('login'))
+        else:
+            return render_template('signup.html')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    error = None
+    if request.method == 'GET':
+        # render login page
+        return render_template('login.html')
+    else: 
+        id = request.form['id'] 
+        pwd = request.form['pwd']
+
+        is_login_success = sp_login(id, pwd)
+        if is_login_success:
+            session['user_id'] = id
+            session['sp'] = StoredProcedures(id)
+            return redirect('/workspace')
+        else:
+            return redirect('/login')
+
+@app.route('/logout', methods=['GET'])
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('index'))
 
 @app.route('/monitoring', methods=['POST'])
 def get_monitoring_data():
+    # aws lambda에서 처리해도 될듯?
     monitoring_data = request.get_json()
     print(monitoring_data)
     return monitoring_data
---
+++ util.py
@@ -0,0 +1,3 @@
+def check_login():
+    "쿠키나 세션에서 로그인 여부를 확인"
+    pass
